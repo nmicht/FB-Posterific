dcb00acea613d3941cf212e694f3d1aba62d6600
Michelle Torres <michelle.torres@corb.mx>
2017-09-16 16:49:04 -0500

    Add button and functions for login with email

--- components/CheckoutScreen.js
+++ components/CheckoutScreen.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Image, View, StyleSheet, Text, ToastAndroid } from 'react-native';
 import { Container, Content, Header, Left, Button, Icon, Body, Title } from 'native-base';
 import PosterModel from '../Model/PosterModel';
+import AccountKit from 'react-native-facebook-account-kit';
 
 export default class CheckoutScreen extends React.Component {
 
@@ -17,7 +18,30 @@ export default class CheckoutScreen extends React.Component {
     super(props);
     this.state = {
       poster: this.props.poster,
+      authToken: null,
+      loggedAccount: null
     };
+  }
+
+  componentWillMount = () => {
+    AccountKit.getCurrentAccessToken()
+      .then((token) => {
+        if (token) {
+          AccountKit.getCurrentAccount()
+            .then((account) => {
+              this.setState({
+                authToken: token,
+                loggedAccount: account
+              });
+              this.logUserPurchase();
+            })
+        } else {
+          console.log("No account logged in");
+        }
+      })
+      .catch((e) => {
+        console.warn("Access token request failed", e);
+      })
   }
 
   render() {
@@ -63,7 +87,7 @@ export default class CheckoutScreen extends React.Component {
               rounded
               style={{ margin: 10 }}
               onPress={() => {
-                this.logUserPurchase();
+                this.loginWithEmail();
               }}
             >
               <Text style={[styles.btnText]}>Buy Now</Text>
@@ -78,6 +102,37 @@ export default class CheckoutScreen extends React.Component {
   logUserPurchase() {
     // assume that the "transaction" succeeded and the purchase was made
     ToastAndroid.showWithGravity("Your purchase was successful", ToastAndroid.LONG, ToastAndroid.CENTER);
+  }
+
+  loginWithEmail() {
+    AccountKit.loginWithEmail()
+      .then((token) => {
+        this.onLoginSuccess(token)
+      })
+      .catch((e) => {
+        this.onLoginError(e)
+      })
+  }
+
+  onLoginSuccess(token) {
+    if (!token) {
+      console.warn("User canceled login");
+      this.setState({});
+    } else {
+      AccountKit.getCurrentAccount()
+        .then((account) => {
+          this.setState({
+            authToken: token,
+            loggedAccount: account
+          })
+          console.log("user already logged in, completing purchase");
+          this.logUserPurchase();
+        })
+    }
+  }
+
+  onLoginError(e) {
+    console.log("Failed to login", e);
   }
 }
 
