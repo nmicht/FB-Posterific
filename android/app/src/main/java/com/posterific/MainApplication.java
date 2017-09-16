65bb01c42cbd39f9fab843bda9d97b3d3efa78dd
Michelle Torres <michelle.torres@corb.mx>
2017-09-16 16:30:52 -0500

    Add account kit dependency

--- android/app/src/main/java/com/posterific/MainApplication.java
+++ android/app/src/main/java/com/posterific/MainApplication.java
@@ -5,6 +5,7 @@ import android.app.Application;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookSdk;
 import com.facebook.react.ReactApplication;
+import io.underscope.react.fbak.RNAccountKitPackage;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -35,6 +36,7 @@ public class MainApplication extends Application implements ReactApplication {
       protected List<ReactPackage> getPackages() {
         return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNAccountKitPackage(),
             new FBSDKPackage(mCallbackManager),
           new RNViewShotPackage(),
           new PickerPackage()
